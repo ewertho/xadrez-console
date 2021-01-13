4ef44e9088ea6612a209e64d5263abda1a226700
eweerr <ewerton.lfb@hotmail.com>
2021-01-13 16:23:25 -0300

    add class: PosicaoXadrez

--- Program.cs
+++ Program.cs
@@ -6,21 +6,11 @@ namespace xadrez_console
     class Program
     {
         static void Main(string[] args)
-        {
-            try{
-                Tabuleiro tab = new Tabuleiro(8,8);
-
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
-
-                Tela.imprimirTabuleiro(tab);
-            }catch(TabuleiroException e){
-                System.Console.WriteLine(e.Message);
-            }
-            
+        {            
+            PosicaoXadrez pos = new PosicaoXadrez('c',7);
+            System.Console.WriteLine(pos);
+            System.Console.WriteLine(pos.ToPosicao());
             Console.ReadLine();
-            
         }
     }
 }
