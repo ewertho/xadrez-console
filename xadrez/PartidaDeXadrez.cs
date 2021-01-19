d5508b9888c846e5b945d1710001c89b51e6b06c
eweerr <ewerton.lfb@hotmail.com>
2021-01-19 14:35:29 -0300

    adding all the pieces

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -157,25 +157,39 @@ namespace xadrez
             pecas.Add(peca);
         }
         private void colocarPeca(){
-            // colocaNovaPeca('c',1, new Torre(tab, Cor.Branca));
-            // colocaNovaPeca('c',2, new Torre(tab, Cor.Branca));
-            // colocaNovaPeca('d',2, new Torre(tab, Cor.Branca));
-            // colocaNovaPeca('e',2, new Torre(tab, Cor.Branca));
-            // colocaNovaPeca('e',1, new Torre(tab, Cor.Branca));
-            // colocaNovaPeca('d',1, new Rei(tab, Cor.Branca));
-
-            // colocaNovaPeca('c',7, new Torre(tab, Cor.Preta));
-            // colocaNovaPeca('c',8, new Torre(tab, Cor.Preta));
-            // colocaNovaPeca('d',7, new Torre(tab, Cor.Preta));
-            // colocaNovaPeca('e',7, new Torre(tab, Cor.Preta));
-            // colocaNovaPeca('e',8, new Torre(tab, Cor.Preta));
-            // colocaNovaPeca('d',8, new Rei(tab, Cor.Preta));
-            colocaNovaPeca('c',1, new Torre(tab, Cor.Branca));
-            colocaNovaPeca('d',1, new Rei(tab, Cor.Branca));
-            colocaNovaPeca('h',7, new Torre(tab, Cor.Branca));
-
-            colocaNovaPeca('a',8, new Rei(tab, Cor.Preta));
-            colocaNovaPeca('b',8, new Torre(tab, Cor.Preta));
+            colocaNovaPeca('a',1, new Torre(tab, Cor.Branca));
+            colocaNovaPeca('h',1, new Torre(tab, Cor.Branca));
+            colocaNovaPeca('b',1, new Cavalo(tab, Cor.Branca));
+            colocaNovaPeca('g',1, new Cavalo(tab, Cor.Branca));
+            colocaNovaPeca('c',1, new Bispo(tab, Cor.Branca));
+            colocaNovaPeca('f',1, new Bispo(tab, Cor.Branca));
+            colocaNovaPeca('d',1, new Rainha(tab, Cor.Branca));
+            colocaNovaPeca('e',1, new Rei(tab, Cor.Branca));
+            colocaNovaPeca('a',2, new Peao(tab, Cor.Branca));
+            colocaNovaPeca('b',2, new Peao(tab, Cor.Branca));
+            colocaNovaPeca('c',2, new Peao(tab, Cor.Branca));
+            colocaNovaPeca('d',2, new Peao(tab, Cor.Branca));
+            colocaNovaPeca('e',2, new Peao(tab, Cor.Branca));
+            colocaNovaPeca('f',2, new Peao(tab, Cor.Branca));
+            colocaNovaPeca('g',2, new Peao(tab, Cor.Branca));
+            colocaNovaPeca('h',2, new Peao(tab, Cor.Branca));
+            //-----------------Pretas------------------------
+            colocaNovaPeca('a',8, new Torre(tab, Cor.Preta));
+            colocaNovaPeca('h',8, new Torre(tab, Cor.Preta));
+            colocaNovaPeca('b',8, new Cavalo(tab, Cor.Preta));
+            colocaNovaPeca('g',8, new Cavalo(tab, Cor.Preta));
+            colocaNovaPeca('c',8, new Bispo(tab, Cor.Preta));
+            colocaNovaPeca('f',8, new Bispo(tab, Cor.Preta));
+            colocaNovaPeca('d',8, new Rainha(tab, Cor.Preta));
+            colocaNovaPeca('e',8, new Rei(tab, Cor.Preta));
+            colocaNovaPeca('a',7, new Peao(tab, Cor.Preta));
+            colocaNovaPeca('b',7, new Peao(tab, Cor.Preta));
+            colocaNovaPeca('c',7, new Peao(tab, Cor.Preta));
+            colocaNovaPeca('d',7, new Peao(tab, Cor.Preta));
+            colocaNovaPeca('e',7, new Peao(tab, Cor.Preta));
+            colocaNovaPeca('f',7, new Peao(tab, Cor.Preta));
+            colocaNovaPeca('g',7, new Peao(tab, Cor.Preta));
+            colocaNovaPeca('h',7, new Peao(tab, Cor.Preta));
         }
     }
 }
---
+++ xadrez/Rainha.cs
@@ -0,0 +1,97 @@
+using tabuleiro;
+namespace xadrez
+{
+    class Rainha : Peca{
+        public Rainha(Tabuleiro tab, Cor cor):base(tab,cor){}
+
+        public override string ToString()
+        {
+            return "D";
+        }
+        private bool podeMover(Posicao pos){
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public override bool[,] movimentosPossiveis(){
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+            Posicao pos = new Posicao(0,0);
+            //TORRE
+            //norte
+            pos.definirValores(posicao.linha-1,posicao.coluna);
+            while(tab.posicaoValida(pos)&& podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+            //sul
+            pos.definirValores(posicao.linha+1,posicao.coluna);
+            while(tab.posicaoValida(pos)&& podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+            //leste
+            pos.definirValores(posicao.linha,posicao.coluna+1);
+            while(tab.posicaoValida(pos)&& podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+            //oeste
+            pos.definirValores(posicao.linha,posicao.coluna-1);
+            while(tab.posicaoValida(pos)&& podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
+
+            //BISPO
+            //nordeste
+            pos.definirValores(posicao.linha -1, posicao.coluna+1);
+            while(tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.definirValores(pos.linha -1, pos.coluna +1);
+            }
+            //sudeste
+            pos.definirValores(posicao.linha +1, posicao.coluna+1);
+            while(tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.definirValores(pos.linha +1, pos.coluna +1);
+            }
+            //sudoeste
+            pos.definirValores(posicao.linha +1, posicao.coluna-1);
+            while(tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.definirValores(pos.linha +1, pos.coluna -1);
+            }
+            //noroeste
+            pos.definirValores(posicao.linha -1, posicao.coluna -1);
+            while(tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.linha, pos.coluna]= true;
+                if(tab.peca(pos) != null && tab.peca(pos).cor != cor){
+                    break;
+                }
+                pos.definirValores(pos.linha -1, pos.coluna -1);
+            }
+            return mat;
+        }
+    }
+}
